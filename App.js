b5308dcf66752dcb80b3f2cdd027f3fcf83a66b0
Noah Johnson <NoahJohnson2@my.unt.edu>
2022-09-27 12:31:59 -0500

    Added some skeleton folders for formatting

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, Button, View, Alert, Text,
+} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    alignItems: 'flex-end',
+    // justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  container__button: {
+    // border: '1px solid black',
+    backgroundColor: '#000',
   },
 });
+
+function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function Settings() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>This is another line of text!!</Text>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={
+          ({ route }) => ({
+            headerShown: false,
+            tabBarIcon: ({ focused, color, size }) => (
+              <Icon name={
+              route.name === 'Home' ? 'list-outline' : 'settings-outline'
+            }
+              />
+            ),
+          })
+}
+        >
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </NavigationContainer>
       <StatusBar />
     </View>
   );
