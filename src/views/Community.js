331da895684108c499fa3c564477946480835df8
Cody Hogan <codyqhogan@gmail.com>
2022-10-09 16:36:45 -0500

    Preliminary commit for taskbar.

---
+++ src/views/Community.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function CommunityTab() {
+  return (
+    <View style={{
+      flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#69A297',
+    }}
+    >
+      <Text>Community Tab</Text>
+    </View>
+  );
+}
---
+++ src/views/Services.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function ServicesTab() {
+  return (
+    <View style={{
+      flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#69A297',
+    }}
+    >
+      <Text>Services Tab</Text>
+    </View>
+  );
+}
