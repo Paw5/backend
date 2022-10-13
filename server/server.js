d15d48d37391c0c874ce1773d29a8c3cd4a32700
KaptainJosh <61091476+KaptainJosh@users.noreply.github.com>
2022-10-13 11:59:51 -0500

    Reorganized code into client and server folders. Added express server to connect to MongoDB Atlas

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
+const port = 3001;
+const uri = process.env.MONGODB_CONNECTION_STRING;
+
+mongoose
+    .connect(uri, { useNewUrlParser: true })
+    .then(() => console.log("MongoDb Connected"));
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.listen(port, () => {console.log("Listening on port 3001")});
