70ebd630288c892e7005a3f572b49a46581af347
KaptainJosh <61091476+KaptainJosh@users.noreply.github.com>
2022-10-13 13:12:10 -0500

    Changed require to import and made connection string just connect to specific database within Atlas cluster

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
-const express = require("express");
+import express from 'express';
 const app = express();
-const mongoose = require("mongoose");
-require("dotenv").config();
+import mongoose from "mongoose";
+//require("dotenv").config();
+import dotenv from "dotenv";
+
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
