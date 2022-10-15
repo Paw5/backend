6a94abaa327e3b3224a172bb5aaff1e5929d5cf2
kgh0054 <107895935+kgh0054@users.noreply.github.com>
2022-10-15 16:13:04 -0500

    Added native font, and edited styles

--- src/views/Health.js
+++ src/views/Health.js
@@ -1,9 +1,12 @@
+/* eslint-disable global-require */
 import {
   Pressable, View, Image, Text, Dimensions, ScrollView, StyleSheet,
 } from 'react-native';
 import React from 'react';
 // import Constants from 'expo-constants';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { useFonts } from 'expo-font';
+import DropShadow from 'react-native-drop-shadow';
 
 const StatusBarHeight = getStatusBarHeight();
 
@@ -25,39 +28,69 @@ const styles = StyleSheet.create({
     margin: 10,
     marginTop: 0,
     marginBottom: -30,
-    borderWidth: 2,
-    borderColor: '#333333',
     borderRadius: 25,
-    elevation: 24,
-    shadowColor: '#000',
-    overflow: 'hidden',
+  },
+
+  shadowProp: {
+    shadowColor: '#333333',
+    shadowOffset: { width: 5, height: 5 },
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
   },
 
   petHeader: {
-    fontFamily: 'arial',
-    fontSize: 25,
-    fontWeight: '300',
+    fontSize: 32,
+    fontWeight: '600',
     textTransform: 'uppercase',
     letterSpacing: 1,
-    marginTop: 10,
+    marginTop: 5,
+    fontFamily: 'QuicksandBold',
+    color: '#333333',
+  },
+
+  petImage: {
+    height: 135,
+    width: 135,
+    borderRadius: 100,
+    borderWidth: 3,
+    borderColor: '#e0777d',
   },
 
   healthContainer: {
-    justifyContent: 'center',
     alignSelf: 'center',
     width: (Dimensions.get('window').width - 20),
     backgroundColor: 'white',
-    borderWidth: 2,
-    borderColor: '#333333',
     borderRadius: 25,
     overflow: 'hidden',
-    alignContent: 'center',
-    height: 300,
     marginBottom: 20,
+  },
+
+  healthHeader: {
+    fontSize: 20,
+    fontWeight: '600',
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+    alignSelf: 'center',
+    marginTop: 15,
+    marginBottom: 5,
+    fontFamily: 'QuicksandBold',
+    color: '#333333',
   },
 });
 
 export default function HealthTab() {
+  const [loaded] = useFonts({
+    QuicksandBold: require('../../assets/fonts/Quicksand-Bold.ttf'),
+    QuicksandLight: require('../../assets/fonts/Quicksand-Light.ttf'),
+    QuicksandMedium: require('../../assets/fonts/Quicksand-Medium.ttf'),
+    QuicksandRegular: require('../../assets/fonts/Quicksand-Regular.ttf'),
+    QuicksandSemiBold: require('../../assets/fonts/Quicksand-SemiBold.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={{
       backgroundColor: '#69A297', flex: 1,
@@ -71,7 +104,11 @@ export default function HealthTab() {
           horizontal
           showsHorizontalScrollIndicator={false}
           snapToAlignment="center"
-          snapToInterval={200}
+          snapToInterval={180}
+          contentOffset={{ x: -110 }}
+          decelerationRate="fast"
+          disableIntervalMomentum
+          directionalLockEnabled
           pagingEnabled
           contentInset={{ left: 100, right: 100 }}
           style={{
@@ -79,51 +116,94 @@ export default function HealthTab() {
           }}
         >
 
-          <Pressable style={styles.petCard}>
-            <Image
-              style={{
-                height: 130, width: 130, borderRadius: 100,
-              }}
-              source={miso}
-            />
-            <Text style={styles.petHeader}>Miso</Text>
+          <DropShadow style={styles.shadowProp}>
+            <Pressable style={styles.petCard}>
+              <Image
+                style={styles.petImage}
+                source={miso}
+              />
+              <Text style={styles.petHeader}>Miso</Text>
 
-          </Pressable>
-          <Pressable style={styles.petCard}>
-            <Image
-              style={{
-                height: 130, width: 130, borderRadius: 100,
-              }}
-              source={miso}
-            />
-            <Text style={styles.petHeader}>Miso</Text>
+            </Pressable>
+          </DropShadow>
+          <DropShadow style={styles.shadowProp}>
+            <Pressable style={styles.petCard}>
+              <Image
+                style={styles.petImage}
+                source={miso}
+              />
+              <Text style={styles.petHeader}>Miso</Text>
 
-          </Pressable>
-          <Pressable style={styles.petCard}>
-            <Image
-              style={{
-                height: 130, width: 130, borderRadius: 100,
-              }}
-              source={miso}
-            />
-            <Text style={styles.petHeader}>Miso</Text>
+            </Pressable>
+          </DropShadow>
+          <DropShadow style={styles.shadowProp}>
+            <Pressable style={styles.petCard}>
+              <Image
+                style={styles.petImage}
+                source={miso}
+              />
+              <Text style={styles.petHeader}>Miso</Text>
 
-          </Pressable>
-          <Pressable style={styles.petCard}>
-            <Image
-              style={{
-                height: 130, width: 130, borderRadius: 100,
-              }}
-              source={miso}
-            />
-            <Text style={styles.petHeader}>Miso</Text>
+            </Pressable>
+          </DropShadow>
+          <DropShadow style={styles.shadowProp}>
+            <Pressable style={styles.petCard}>
+              <Image
+                style={styles.petImage}
+                source={miso}
+              />
+              <Text style={styles.petHeader}>Miso</Text>
 
-          </Pressable>
+            </Pressable>
+          </DropShadow>
         </ScrollView>
 
-        <Pressable style={styles.healthContainer} />
-        <Pressable style={styles.healthContainer} />
-        <Pressable style={styles.healthContainer} />
+        <DropShadow style={styles.shadowProp}>
+          <Pressable style={[styles.healthContainer, { height: 200 }]}>
+            <Text style={styles.healthHeader}>Upcoming Appointments</Text>
+            <View
+              style={{
+                borderBottomColor: '#e0777d',
+                borderBottomWidth: 3,
+                borderRadius: 50,
+                marginLeft: 10,
+                marginRight: 10,
+              }}
+            />
+          </Pressable>
+
+        </DropShadow>
+        <DropShadow style={styles.shadowProp}>
+          <Pressable style={[styles.healthContainer, { height: 150 }]}>
+            <Text style={styles.healthHeader}>Did you feed the dog today?</Text>
+            <View
+              style={{
+                borderBottomColor: '#e0777d',
+                borderBottomWidth: 3,
+                borderRadius: 50,
+                marginLeft: 10,
+                marginRight: 10,
+              }}
+            />
+          </Pressable>
+
+        </DropShadow>
+        <DropShadow style={styles.shadowProp}>
+          <Pressable style={[styles.healthContainer, { height: 300 }]}>
+            <Text style={styles.healthHeader}>Walk Tracker</Text>
+            <View
+              style={{
+                borderBottomColor: '#e0777d',
+                borderBottomWidth: 3,
+                borderRadius: 50,
+                marginLeft: 10,
+                marginRight: 10,
+              }}
+            />
+          </Pressable>
+
+        </DropShadow>
+
       </ScrollView>
     </View>
   );
