48943f0b5ef202e3711fe5900c105b17eb842a9d
kgh0054 <107895935+kgh0054@users.noreply.github.com>
2022-10-18 12:08:30 -0500

    Added Account and Settings Content

--- src/views/Account.js
+++ src/views/Account.js
@@ -1,14 +1,162 @@
-import { View, Text } from 'react-native';
+/* eslint-disable global-require */
+import {
+  View, Text, Dimensions, StyleSheet, Pressable, Image,
+} from 'react-native';
 import React from 'react';
-import Constants from 'expo-constants';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { useFonts } from 'expo-font';
+import DropShadow from 'react-native-drop-shadow';
+import { Feather } from '@expo/vector-icons';
 
-export default function SettingsPage() {
+const StatusBarHeight = getStatusBarHeight();
+const miso = require('../../assets/miso.jpg');
+
+const styles = StyleSheet.create({
+  profileBorder: {
+    backgroundColor: 'white',
+    height: 220,
+    width: 220,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    borderRadius: 150,
+    marginBottom: 20,
+  },
+  profileImage: {
+    height: 200,
+    width: 200,
+    alignSelf: 'center',
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  menuItem: {
+    alignSelf: 'center',
+    width: (Dimensions.get('window').width - 20),
+    backgroundColor: 'white',
+    borderRadius: 25,
+    overflow: 'hidden',
+    marginBottom: 20,
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 18,
+    paddingRight: 20,
+  },
+  shadowProp: {
+    shadowColor: '#333333',
+    shadowOffset: { width: 5, height: 5 },
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
+  },
+  menuText: {
+    fontSize: 24,
+    width: (Dimensions.get('window').width - 120),
+    fontWeight: '600',
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+    fontFamily: 'QuicksandBold',
+    color: '#333333',
+    flexBasis: 'auto',
+  },
+  lightdark: {
+    flexBasis: 'auto',
+  },
+});
+
+export default function ServicesTab() {
+  const [loaded] = useFonts({
+    QuicksandBold: require('../../assets/fonts/Quicksand-Bold.ttf'),
+    QuicksandLight: require('../../assets/fonts/Quicksand-Light.ttf'),
+    QuicksandMedium: require('../../assets/fonts/Quicksand-Medium.ttf'),
+    QuicksandRegular: require('../../assets/fonts/Quicksand-Regular.ttf'),
+    QuicksandSemiBold: require('../../assets/fonts/Quicksand-SemiBold.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={{
-      flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#69A297', paddingTop: Constants.statusBarHeight,
+      flex: 1, backgroundColor: '#69A297',
     }}
     >
-      <Text>Account Page</Text>
+
+      <View style={{ backgroundColor: '#e0777d', height: StatusBarHeight, marginBottom: 30 }} />
+      <View>
+        <DropShadow style={styles.shadowProp}>
+          <View style={styles.profileBorder}>
+            <Image
+              style={styles.profileImage}
+              source={miso}
+            />
+          </View>
+        </DropShadow>
+      </View>
+      <DropShadow style={styles.shadowProp}>
+        <View style={[styles.menuItem, { marginBottom: 50, justifyContent: 'center', backgroundColor: '#e0777d' }]}>
+          <Text
+            adjustsFontSizeToFit
+            numberOfLines={1}
+            style={[styles.menuText, { width: 'auto', fontSize: 32, color: 'white' }]}
+          >
+            UserName
+          </Text>
+
+        </View>
+
+      </DropShadow>
+      <DropShadow style={styles.shadowProp}>
+        <Pressable style={styles.menuItem}>
+          <Text
+            adjustsFontSizeToFit
+            numberOfLines={1}
+            style={styles.menuText}
+          >
+            Edit Profile
+          </Text>
+          <Feather
+            name="chevron-right"
+            size={30}
+            color="#333333"
+            style={{ marginRight: -5 }}
+          />
+
+        </Pressable>
+
+      </DropShadow>
+      <DropShadow style={styles.shadowProp}>
+        <Pressable style={styles.menuItem}>
+          <Text
+            adjustsFontSizeToFit
+            numberOfLines={1}
+            style={styles.menuText}
+          >
+            Edit Pets
+          </Text>
+          <Feather
+            name="chevron-right"
+            size={30}
+            color="#333333"
+            style={{ marginRight: -5 }}
+          />
+
+        </Pressable>
+
+      </DropShadow>
+      <DropShadow style={styles.shadowProp}>
+        <Pressable style={styles.menuItem}>
+          <Text
+            adjustsFontSizeToFit
+            numberOfLines={1}
+            style={styles.menuText}
+          >
+            Sign out
+          </Text>
+
+        </Pressable>
+
+      </DropShadow>
     </View>
   );
 }
--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,15 +1,25 @@
 /* eslint-disable global-require */
 import {
-  View, Text, Dimensions, StyleSheet, Pressable,
+  View, Text, Dimensions, StyleSheet, Switch, Pressable,
 } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 import { useFonts } from 'expo-font';
 import DropShadow from 'react-native-drop-shadow';
+import { Feather } from '@expo/vector-icons';
 
 const StatusBarHeight = getStatusBarHeight();
 
 const styles = StyleSheet.create({
+  settingsIcon: {
+    alignSelf: 'center',
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 70,
+    overflow: 'hidden',
+    marginBottom: 60,
+    marginTop: 20,
+  },
   menuItem: {
     alignSelf: 'center',
     width: (Dimensions.get('window').width - 20),
@@ -17,7 +27,12 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     overflow: 'hidden',
     marginBottom: 20,
-    height: 50,
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 18,
+    paddingRight: 20,
   },
   shadowProp: {
     shadowColor: '#333333',
@@ -26,19 +41,26 @@ const styles = StyleSheet.create({
     shadowRadius: 2,
   },
   menuText: {
-    fontSize: 20,
+    fontSize: 24,
+    width: (Dimensions.get('window').width - 120),
     fontWeight: '600',
     textTransform: 'uppercase',
     letterSpacing: 1,
-    alignSelf: 'center',
-    marginTop: 15,
-    marginBottom: 5,
     fontFamily: 'QuicksandBold',
     color: '#333333',
+    flexBasis: 'auto',
+  },
+  lightdark: {
+    flexBasis: 'auto',
   },
 });
 
 export default function ServicesTab() {
+  const [LDisEnabled, LSsetIsEnabled] = useState(false);
+  const [LCisEnabled, LCsetIsEnabled] = useState(false);
+  const lightdarkSwitch = () => LSsetIsEnabled((previousState) => !previousState);
+  const locationSwitch = () => LCsetIsEnabled((previousState) => !previousState);
+
   const [loaded] = useFonts({
     QuicksandBold: require('../../assets/fonts/Quicksand-Bold.ttf'),
     QuicksandLight: require('../../assets/fonts/Quicksand-Light.ttf'),
@@ -50,25 +72,99 @@ export default function ServicesTab() {
   if (!loaded) {
     return null;
   }
+
   return (
     <View style={{
       flex: 1, backgroundColor: '#69A297',
     }}
     >
 
-      <View style={{ backgroundColor: '#e0777d', height: StatusBarHeight }} />
+      <View style={{ backgroundColor: '#e0777d', height: StatusBarHeight, marginBottom: 30 }} />
+      <View>
+        <DropShadow style={styles.shadowProp}>
+          <Feather
+            name="settings"
+            size={100}
+            color="#333333"
+            style={styles.settingsIcon}
+          />
+        </DropShadow>
+      </View>
+      <DropShadow style={styles.shadowProp}>
+        <View style={styles.menuItem}>
+          <Text
+            adjustsFontSizeToFit
+            numberOfLines={1}
+            style={styles.menuText}
+          >
+            Dark/Light Mode
+          </Text>
+          <Switch
+            style={styles.lightdark}
+            trackColor={{ false: '#e0777d', true: '#edae49' }}
+            thumbColor="white"
+            ios_backgroundColor="#e0777d"
+            onValueChange={lightdarkSwitch}
+            value={LDisEnabled}
+          />
+        </View>
+
+      </DropShadow>
+      <DropShadow style={styles.shadowProp}>
+        <View style={styles.menuItem}>
+          <Text
+            adjustsFontSizeToFit
+            numberOfLines={1}
+            style={styles.menuText}
+          >
+            Disable Location Services
+          </Text>
+          <Switch
+            style={styles.lightdark}
+            trackColor={{ false: '#e0777d', true: '#333333' }}
+            thumbColor="white"
+            ios_backgroundColor="#e0777d"
+            onValueChange={locationSwitch}
+            value={LCisEnabled}
+          />
+        </View>
+
+      </DropShadow>
       <DropShadow style={styles.shadowProp}>
         <Pressable style={styles.menuItem}>
-          <Text style={styles.menuText}>Accessibility Features</Text>
-          <View
-            style={{
-              borderBottomColor: '#e0777d',
-              borderBottomWidth: 3,
-              borderRadius: 50,
-              marginLeft: 10,
-              marginRight: 10,
-            }}
+          <Text
+            adjustsFontSizeToFit
+            numberOfLines={1}
+            style={styles.menuText}
+          >
+            Notifications
+          </Text>
+          <Feather
+            name="chevron-right"
+            size={30}
+            color="#333333"
+            style={{ marginRight: -5 }}
           />
+
+        </Pressable>
+
+      </DropShadow>
+      <DropShadow style={styles.shadowProp}>
+        <Pressable style={styles.menuItem}>
+          <Text
+            adjustsFontSizeToFit
+            numberOfLines={1}
+            style={styles.menuText}
+          >
+            Help
+          </Text>
+          <Feather
+            name="chevron-right"
+            size={30}
+            color="#333333"
+            style={{ marginRight: -5 }}
+          />
+
         </Pressable>
 
       </DropShadow>
