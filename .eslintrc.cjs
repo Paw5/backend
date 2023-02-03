da502c55a93d3f6dbf15baa44452f5073a40ab50
johnsnoa <johnson.noah.b@gmail.com>
2023-02-02 22:38:49 -0600

    eslintrc update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'jest',
   ],
   parserOptions: {
     ecmaFeatures: {
