fe11107558e111c434cc2d68a397b5588edb75b5
johnsnoa <johnson.noah.b@gmail.com>
2023-02-03 02:30:37 -0600

    Marked jest config as cjs

---
+++ jest.config.cjs
@@ -0,0 +1,20 @@
+const config = {
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/jest.config.js',
+    '!**/coverage/**',
+  ],
+  coverageThreshold: {
+    global: {
+      functions: 90,
+      statements: -10,
+    },
+  },
+  transform: {},
+};
+
+module.exports = config;
