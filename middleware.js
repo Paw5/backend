800f14773df5c764cf7ecb859808da7eed82247f
KaptainJosh <61091476+KaptainJosh@users.noreply.github.com>
2023-03-05 19:44:47 -0600

    Disabled feature flags feature

--- middleware.js
+++ middleware.js
@@ -14,24 +14,25 @@ const requireHTTPS = (req, res, next) => {
 };
 
 const requireEnabledEndpoint = (req, res, next) => {
-  const { url } = req;
-  let urlParts = url.split('?')[0].split('/').slice(1);
-  let scanEndpoints = endpoints;
-  if (scanEndpoints[urlParts[0]]) {
-    while (urlParts[0]) {
-      scanEndpoints = scanEndpoints[urlParts[0]];
-      urlParts = urlParts.slice(1);
-    }
-    if (scanEndpoints && scanEndpoints.verbs) {
-      next();
-    } else {
-      console.error('Middleware requireEnabledEndpoint failed');
-      res.status(405).send('<h1>405 Method Not Allowed</h1>');
-    }
-  } else {
-    console.error('Middleware requireEnabledEndpoint failed');
-    res.status(405).send('<h1>405 Method Not Allowed</h1>');
-  }
+  next();
+  // const { url } = req;
+  // let urlParts = url.split('?')[0].split('/').slice(1);
+  // let scanEndpoints = endpoints;
+  // if (scanEndpoints[urlParts[0]]) {
+  //   while (urlParts[0]) {
+  //     scanEndpoints = scanEndpoints[urlParts[0]];
+  //     urlParts = urlParts.slice(1);
+  //   }
+  //   if (scanEndpoints && scanEndpoints.verbs) {
+  //     next();
+  //   } else {
+  //     console.error('Middleware requireEnabledEndpoint failed');
+  //     res.status(405).send('<h1>405 Method Not Allowed</h1>');
+  //   }
+  // } else {
+  //   console.error('Middleware requireEnabledEndpoint failed');
+  //   res.status(405).send('<h1>405 Method Not Allowed</h1>');
+  // }
 };
 
 const responseHeaders = (req, res, next) => {
