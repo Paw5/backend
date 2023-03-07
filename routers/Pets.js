b4172e0423fb5a77a8ce7d681a7a09307fbf8cef
KaptainJosh <61091476+KaptainJosh@users.noreply.github.com>
2023-03-07 17:10:40 -0600

    Updated to account for no params given to filter

--- routers/Pets.js
+++ routers/Pets.js
@@ -21,8 +21,11 @@ export const prepareQuery = (fields, limit, page, filterParams) => {
     limitSql += ` OFFSET ${sqlOffset}`;
   }
 
-  const combinedEntries = Object.entries(filterParams).map(([field, value]) => `${field}=?`).join(' AND ');
-  const whereQuery = `WHERE ${combinedEntries}`;
+  let whereQuery = '';
+  if (Object.keys(filterParams).length > 0) {
+    const combinedEntries = Object.entries(filterParams).map(([field, value]) => `${field}=?`).join(' AND ');
+    whereQuery = `WHERE ${combinedEntries}`;
+  };
 
   const query = `SELECT ${sqlFields} FROM pets ${whereQuery} ${limitSql}`;
   
@@ -34,10 +37,10 @@ router.get('/', (req, res) => {
     fields, limit, page, ...filterParams
   } = req.query;
   const query = prepareQuery(fields, limit, page, filterParams);
-  console.log(query);
+  //console.log(query);
 
   const combinedValues = Object.entries(filterParams).map(([field, value]) => value);
-  console.log(combinedValues);
+  //console.log(combinedValues);
   connection.query(query, combinedValues)
     .then((results) => res.json({ results: results[0] }))
     .catch(() => res.status(400).json({ results: [] }));
