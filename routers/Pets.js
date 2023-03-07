a53c4d1f16907990f7b94f444c96fa99d90e399e
KaptainJosh <61091476+KaptainJosh@users.noreply.github.com>
2023-03-07 16:55:13 -0600

    Changed how filterParams is used in query

--- routers/Pets.js
+++ routers/Pets.js
@@ -6,41 +6,39 @@ const connection = Database();
 
 const router = Router();
 
-export const prepareQuery = (req) => {
-  const {
-    filterParams, fields, limit, page,
-  } = req.query;
+export const prepareQuery = (fields, limit, page, filterParams) => {
   let sqlOffset = 0;
   sqlOffset += ((Number(page) || 1) - 1) * (Number(limit) || 20);
-
-  // const columns = {
-  //   pet_details: pet,
-  // }[mode] || '*';
 
   let sqlFields = fields;
 
   if (fields === '') {
     sqlFields = '*';
-  } 
+  }
   
   let limitSql = `LIMIT ${Number(limit) || 20}`;
   if (sqlOffset) {
     limitSql += ` OFFSET ${sqlOffset}`;
   }
 
-  let filterSql = '';
-  if (filterParams !== '') {
-    filterSql = ` WHERE ${filterParams}`;
-  }
+  const combinedEntries = Object.entries(filterParams).map(([field, value]) => `${field}=?`).join(' AND ');
+  const whereQuery = `WHERE ${combinedEntries}`;
 
-  const query = `SELECT ${sqlFields} FROM pets ${filterSql} ${limitSql}`;
+  const query = `SELECT ${sqlFields} FROM pets ${whereQuery} ${limitSql}`;
   
   return query;
 };
 
 router.get('/', (req, res) => {
-  const query = prepareQuery(req);
-  connection.query(query)
+  const {
+    fields, limit, page, ...filterParams
+  } = req.query;
+  const query = prepareQuery(fields, limit, page, filterParams);
+  console.log(query);
+
+  const combinedValues = Object.entries(filterParams).map(([field, value]) => value);
+  console.log(combinedValues);
+  connection.query(query, combinedValues)
     .then((results) => res.json({ results: results[0] }))
     .catch(() => res.status(400).json({ results: [] }));
 });
