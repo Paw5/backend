568c6aecff6f1f9dff51275eac7585129c19e080
johnsnoa <johnson.noah.b@gmail.com>
2023-03-14 13:10:05 -0500

    Added PATCH for pets/userId/petId

--- routers/Pets.js
+++ routers/Pets.js
@@ -13,35 +13,32 @@ export const prepareQuery = (fields, limit, page, filterParams) => {
   let sqlOffset = 0;
   sqlOffset += ((Number(page) || 1) - 1) * (Number(limit) || 20);
 
+  let sqlFields = '*';
 
-  const fields1 = fields.split(','); // splits the single string with all the fields into multiple strings for each field held in an array
-  const fields2 = fields1.map(field => field.trim()); // removes whitespace from the front of each field (neccessary when having more than one field)
-  const fields3 = fields2.map(field => escapeId(field)); // escapes each field
-  
-  let sqlFields = fields3.join(); // combines escaped fields back into one string to be used for query
+  if (fields) {
+    if (typeof (fields) !== 'string') throw new TypeError('Invalid fields type');
+    const fieldsArray = fields.split(','); // splits the single string with all the fields into multiple strings for each field held in an array
+    const fieldsEscaped = fieldsArray.map((field) => escapeId(field.trim()));
 
-  // checks if there are no fields given. if so, it just selects all columns
-  if (fields === '') {
-    sqlFields = '*';
+    sqlFields = fieldsEscaped.join();
   }
-  
+
   let limitSql = `LIMIT ${Number(limit) || 20}`; // sets the limit to be either what the client specifies or 20
-  
+
   // if there is an offset provided by the client, add it to the query
   if (sqlOffset) {
     limitSql += ` OFFSET ${sqlOffset}`;
   }
 
   let whereQuery = ''; // holds the where part of the query
-  
-  // if there are filter parameters given then go through and form a where query using placeholder notation
+
   if (Object.keys(filterParams).length > 0) {
-    const combinedEntries = Object.entries(filterParams).map(([field, value]) => `${field}=?`).join(' AND ');
+    const combinedEntries = Object.entries(filterParams).map(([field]) => `${field}=?`).join(' AND ');
     whereQuery = `WHERE ${combinedEntries}`;
   }
 
   const query = `SELECT ${sqlFields} FROM pets ${whereQuery} ${limitSql}`; // assemble the query together
-  
+
   return query; // return the query
 };
 
@@ -50,10 +47,10 @@ router.get('/', (req, res) => {
     fields, limit, page, ...filterParams
   } = req.query;
   const query = prepareQuery(fields, limit, page, filterParams);
-  //console.log(query);
+  // console.log(query);
 
-  const combinedValues = Object.entries(filterParams).map(([field, value]) => value);
-  //console.log(combinedValues);
+  const combinedValues = Object.entries(filterParams).map(([, value]) => value);
+  // console.log(combinedValues);
   connection.query(query, [combinedValues])
     .then((results) => res.json({ results: results[0] }))
     .catch(() => res.status(400).json({ results: [] }));
@@ -95,7 +92,60 @@ router.post('/:user_id', async (req, res) => {
     connection
       .query('SELECT * FROM pets WHERE user_id = ? AND pet_id = LAST_INSERT_ID()', userId)
       .then(([results]) => {
-        res.json(results[0]);
+        res.json(results[0][0]);
+      });
+  } catch (e) {
+    console.error(e);
+    if (e.fatal) {
+      res.sendStatus(500);
+      throw e;
+    }
+    res.sendStatus(400);
+  }
+});
+
+router.patch('/:user_id/:pet_id', async (req, res) => {
+  const fields = [
+    'pet_name',
+    'type',
+    'breed',
+    'fur_color',
+    'weight',
+    'microchip',
+    'custom_info',
+  ];
+  const { params } = req;
+
+  if (typeof req.body !== 'object') {
+    res.status(400).send('Body must be an object or JSON-parsable string');
+    return;
+  }
+
+  params.user_id = Number(params.user_id);
+  params.pet_id = Number(params.pet_id);
+
+  if (!Number.isSafeInteger(params.user_id)) {
+    res.status(400).send('User ID must be an integer');
+    return;
+  }
+  if (!Number.isSafeInteger(params.pet_id)) {
+    res.status(400).send('Pet ID must be an integer');
+    return;
+  }
+
+  const requestFields = Object.entries(req.body)
+    .filter((entry) => fields.includes(entry[0]));
+
+  const fieldString = requestFields.map(([k]) => `${k}=?`).join(', ');
+
+  try {
+    await connection.query('UPDATE pets SET '
+      + `${fieldString} `
+      + 'WHERE user_id=? AND pet_id=?', [...requestFields.map(([, v]) => v), params.user_id, params.pet_id]);
+    connection
+      .query('SELECT * FROM pets WHERE user_id = ? AND pet_id = ?', [params.user_id, params.pet_id])
+      .then(([results]) => {
+        res.json(results[0][0]);
       });
   } catch (e) {
     console.error(e);
